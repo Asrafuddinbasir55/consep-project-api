68c3ebbef8f6f130eef1daf2ce6802545932787b
Asrafuddinbasir55 <asrafuddinbasir0@gamil.com>
2022-02-27 03:32:24 +0600

    add commit

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 
 const allPlayers = () => {
-    const searchValue = document.getElementById('input-box');
+    document.getElementById('player-container').innerHTML = ''
+    document.getElementById('sppiner').style.display = 'block'
+    const searchValue = document.getElementById('input-box').value;
 
-    const url = `https://www.thesportsdb.com/api/v1/json/2/searchevents.php?e=${searchValue}`;
-    console.log(url);
-    // const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`;
+    const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`;
+    // console.log(url);
     fetch(url)
     .then(res => res.json())
-    .then(data => showPlyerDetaile(data))
-    // console.log(searchValue);   
-}
+    .then(data => {
+        console.log(data.player == null);
+        if (data.player == null) {
+          document.getElementById('sppiner').style.display = 'block'
+        }
+        else {
+            showPlyerDetaile(data.player)
+            document.getElementById('sppiner').style.display = 'none'
+        } 
+    })
 
-const showPlyerDetaile = (players) => {
-    console.log(players);
+
+    //  console.log(searchValue);   
+}
+const showPlyerDetaile = (players) => { 
    for (const player of players) {
-       
+     console.log(player);
     const parent = document.getElementById('player-container');
     const div = document.createElement('div')
     div.innerHTML = `
-    <div class="card border">
+    <div class="card border p-5">
        <div class="pro-pic w-25">
-        <img class="w-25" src="${player.strThumb}" alt="">
+        <img class="w-50" src="${player.strThumb}" alt="">
 
        </div>
        <h2>Name: ${player.strPlayer} </h2>
@@ -28,28 +38,40 @@ const showPlyerDetaile = (players) => {
        <p></p>
        <div class="all-button">
            <button class="btn btn-danger">Delete</button>
-           <button onclick="details('${player.Idplayer}')" class="btn btn-success">Details</button>
+           <button onclick="details('${player.idPlayer}')" class="btn btn-success">Details</button>
        </div>
    </div>
     `
     parent.appendChild(div)
-   }
-    // console.log(player);
+  }
 }
-
 const details = (id) => {
+//   console.log(info.strGender);
  const url = `https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${id}`
  fetch(url)
  .then(res => res.json())
- .then(data => console.log(data.players[0]))
+ .then(data => setDetails(data.players[0]))
 }
 
 const setDetails = (info) => {
+    console.log(info.strGender);
+  if(info.strGender == 'Male') {
+    document.getElementById('male').style.display = 'block'
+    document.getElementById('female').style.display = 'none'
+  }
+  else {
+    document.getElementById('male').style.display = 'none'
+    document.getElementById('female').style.display = 'block' 
+  }
+
+
+
+
     document.getElementById('details-container').innerHTML = `
     <div>
     <img src="" alt="">
     <h1>Name: ${info.strPlayer}</h1>
     </div>
     `
-    console.log(info);
+    // console.log(info);
 }
